c88737d4c1ebc34ae9f9f4a545729709f1b3436f
Teroxlab <emmanuels820@gmail.com>
2023-07-04 22:57:27 +0200

    Graham's Scan Algorithm

--- Graham-s-Scan-Algorithm.py
+++ Graham-s-Scan-Algorithm.py
@@ -1,38 +1,59 @@
 import matplotlib.pyplot as plt
 import math
-def merge_sort(points, key='x'):
+def merge_sort(points, key='p0'):
+    """
+    Sorts a list of points based on their polar angle with respect to p0.
+    Args:points (list): List of points to be sorted.
+    p0 (tuple): Origin point with respect to which the polar angles are calculated.
+    Returns:list: Sorted list of points.
+    """
+
+    # Base case: If the list has 0 or 1 element, it is already sorted
     if len(points) <= 1:
         return points
 
+    # Divide the list into two portions
     mid = len(points) // 2
     left_half = points[:mid]
     right_half = points[mid:]
 
+    # Sorting the left and right portions recursively
     left_sorted = merge_sort(left_half, key)
     right_sorted = merge_sort(right_half, key)
 
+    # Merging the sorted portions
     return merge(left_sorted, right_sorted, key)
 
 def merge(left, right, key):
-    merged = []
+    """
+    Merges two sorted lists of points based on their polar angles with respect to p0.
+    Args:
+    left (list): Sorted list of points.
+    right (list): Sorted list of points.
+    p0 (tuple): Origin point with respect to which the polar angles are calculated.
+    Returns:
+    list: Merged and sorted list of points.
+    """
+
+    merged = []  # Listing to store the merged points
     left_index = right_index = 0
 
+    # Comparing the polar angles of the points in the left and right lists
     while left_index < len(left) and right_index < len(right):
-        if key == 'x':
-            if left[left_index][0] <= right[right_index][0]:
-                merged.append(left[left_index])
-                left_index += 1
-            else:
-                merged.append(right[right_index])
-                right_index += 1
-        elif key == 'y':
-            if left[left_index][1] <= right[right_index][1]:
-                merged.append(left[left_index])
-                left_index += 1
-            else:
-                merged.append(right[right_index])
-                right_index += 1
 
+        # Compute the polar angles of the points
+        left_angle = polar_angle(left[left_index], key)
+        right_angle = polar_angle(right[right_index], key)
+
+        # Comparing the angles and append the point with the smaller angle to the merged list
+        if left_angle <= right_angle:
+            merged.append(left[left_index])
+            left_index += 1
+        else:
+            merged.append(right[right_index])
+            right_index += 1
+
+    # Appending the remaining points from the left and right lists (if any)
     while left_index < len(left):
         merged.append(left[left_index])
         left_index += 1
@@ -40,11 +61,80 @@ def merge(left, right, key):
     while right_index < len(right):
         merged.append(right[right_index])
         right_index += 1
-
     return merged
 
-def distance(P1, P2):
+def polar_angle(point, key):
     """
-    Calculate the distance between two points P1 and P2.
+    Computing the polar angle of a point with respect to p0.
     """
-    return math.sqrt((P1[0] - P2[0]) ** 2 + (P1[1] - P2[1]) ** 2)
+    dx = point[0] - key[0]
+    dy = point[1] - key[1]
+    return math.atan2(dy, dx)
+
+def graham_scan_algorithm(points):
+    """
+    Computes the convex hull of a set of points using Graham's Scan algorithm.
+    Args:
+    points (list): List of points.
+    Returns:
+    list: Convex hull points in counterclockwise order.
+    """
+
+    # Find the point with the lowest y-coordinate (smallest y, ties broken by smallest x)
+    lowest_point = min(points, key=lambda point: (point[1], point[0]))
+
+    # Sort the points by their polar angles with respect to the lowest point
+    sorted_points = sorted(points, key=lambda point: polar_angle(point, lowest_point))
+
+    # Initialize the stack with the lowest and second-lowest points
+    stack = [lowest_point, sorted_points[1]]
+
+    # Iterate over the remaining sorted points
+    for point in sorted_points[2:]:
+        while len(stack) > 1 and is_clockwise(stack[-2], stack[-1], point):
+            stack.pop()
+
+        stack.append(point)
+
+    return stack
+
+def is_clockwise(p1, p2, p3):
+    """
+    Checks if the points p1, p2, and p3 form a clockwise turn.
+
+    Args:
+        p1 (tuple): First point.
+        p2 (tuple): Second point.
+        p3 (tuple): Third point.
+
+    Returns:
+        bool: True if the points form a clockwise turn, False otherwise.
+    """
+
+    return (p2[1] - p1[1]) * (p3[0] - p2[0]) > (p2[0] - p1[0]) * (p3[1] - p2[1])
+
+# Example usage
+points = [(1, 2), (-3, 4), (5, 6), (7, 8), (9, 10)]
+p0 = (0, 0)
+
+# Sort the points based on their polar angles with respect to p0
+sorted_points = merge_sort(points, p0)
+
+# Compute the convex hull using Graham's Scan algorithm
+convex_hull = graham_scan(sorted_points)
+
+# Extract the x and y coordinates of the sorted points and convex hull points
+x_sorted = [point[0] for point in sorted_points]
+y_sorted = [point[1] for point in sorted_points]
+x_hull = [point[0] for point in convex_hull]
+y_hull = [point[1] for point in convex_hull]
+
+# Plot the sorted points and the convex hull
+plt.scatter(x_sorted, y_sorted, c='blue', label='Sorted Points')
+plt.scatter(x_hull, y_hull, c='red', label='Convex Hull')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Convex Hull')
+plt.legend()
+plt.grid(True)
+plt.show()
